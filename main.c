3d21aaa8f44fb96e0d4251e72f46ac93047fcbfc
Petr Helebrant <phelebra@c2r4s5.42prague.com>
2023-01-10 16:57:14 +0100

    ft_strlen

---
+++ main.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 09:05:21 by phelebra          #+#    #+#             */
+/*   Updated: 2023/01/10 15:09:10 by phelebra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "ft_isalpha.c"
+#include "ft_isdigit.c"
+#include "ft_isalnum.c"
+#include "ft_isascii.c"
+#include "ft_isprint.c"
+#include "ft_strlen.c"
+
+int main()
+{
+	//ft_isalpha
+	char c;
+
+	c = '1';
+	printf("\nCheck ft_isaplha on char 1: %d", ft_isalpha(c));
+	c = 'A';
+	printf("\nCheck ft_isaplha on char A: %d\n", ft_isalpha(c));
+
+	//ft_isdigit
+	c = '1';
+	printf("\nCheck ft_isdigit on char 1: %d", ft_isdigit(c));
+	c = 'A';
+	printf("\nCheck ft_isdigit on char A: %d\n", ft_isdigit(c));
+
+	//ft_isalnum
+	c = '1';
+	printf("\nCheck ft_isalnum on char 1: %d", ft_isalnum(c));
+	c = 'A';
+	printf("\nCheck ft_isalnum on char A: %d\n", ft_isalnum(c));
+
+	//ft_isascii
+	c = '	';
+	printf("\nCheck ft_isascii on char TAB: %d", ft_isascii(c));
+	c = 'A';
+	printf("\nCheck ft_isascii on char A: %d", ft_isascii(c));
+	c = '四';
+	printf("\nCheck ft_isascii on char 四: %d", ft_isascii(c));
+	c = '二';
+	printf("\nCheck ft_isascii on char 二: %d\n", ft_isascii(c));
+
+	//ft_isprint
+	c = '	';
+	printf("\nCheck ft_isprint on char TAB: %d", ft_isprint(c));
+	c = 'A';
+	printf("\nCheck ft_isprint on char A: %d", ft_isprint(c));
+	c = '四';
+	printf("\nCheck ft_isprint on char 四: %d", ft_isprint(c));
+	c = '二';
+	printf("\nCheck ft_isprint on char 二: %d\n", ft_isprint(c));
+
+	//ft_strlen
+	char str[50];
+	int len;
+	strcpy(str, "42 Prague");
+	len = ft_strlen(str);
+	printf("\nLength of \"%s\" is %d\n", str, len);
+	
+}
+
