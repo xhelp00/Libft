6ec0d0e110e51fa209b949dd852b50468a04ffd4
Petr Helebrant <phelebra@c2r4s5.42prague.com>
2023-01-13 16:35:50 +0100

    mem functions finished

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 • bzero
-• memcpy
-• memmove
 • strlcpy
 • strlcat
 • strchr
 • strrchr
-• strncmp
-• memchr - to be tested
-• memcmp
 • strnstr
 • calloc
 • strdup
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,8 +6,23 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:03:08 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/13 10:31:48 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/13 15:42:56 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dest & !src)
+		return (0);
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
