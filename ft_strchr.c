65e4520ac61294fb5f487f797de05a25b617c8ba
xhelp00 <xhelp00@gmail.com>
2023-01-15 16:20:51 +0100

    added ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,8 +6,19 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:05:14 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/13 10:32:00 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/15 16:18:39 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+		{
+			if (!*s)
+				return (0);
+			s++;
+		}
+	return ((char *)s);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 10:17:52 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/15 11:55:28 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/15 15:52:25 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 char 	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
