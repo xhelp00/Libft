985a6b3a432b40d8809144b2f28a5a95b6af8486
Petr Helebrant <phelebra@c2r4s5.42prague.com>
2023-01-16 08:55:34 +0100

    improved ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,24 +6,22 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:03:56 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/15 21:56:40 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/16 08:54:36 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstbuffsize)
 {
 	size_t	i;
 	size_t	srcsize;
 
 	i = 0;
-	/* if (dst[i] == '\0' || src[i] == '\0')
-		return (0); */
 	srcsize = ft_strlen(src);
-	if (dstsize != 0)
+	if (dstbuffsize != 0)
 	{
-		while (src[i] != '\0' && i < (dstsize - 1))
+		while (src[i] != '\0' && i < (dstbuffsize - 1))
 		{
 			dst[i] = src[i];
 			i++;
