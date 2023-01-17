86ac75da21cc3bbcbec15464a44367c6971dbde8
Petr Helebrant <phelebra@c2r4s5.42prague.com>
2023-01-17 13:57:35 +0100

    added ft_putchar_fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,8 +6,13 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 10:49:41 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/13 10:50:52 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:56:04 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: phelebra <xhelp00@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 10:17:52 by phelebra          #+#    #+#             */
-/*   Updated: 2023/01/16 16:56:09 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:56:26 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
